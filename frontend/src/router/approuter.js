c6d282365db7e78ac7c3ca6bec8dbd55e819ada2
thenullterminator <dazz2803@gmail.com>
2019-08-09 00:53:20 +0530

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit f7621bc0230171523fb8c6a36b39cf6c3f058160, reversing
    changes made to 2738d60640389284582a0cec7a4659dfb6b93e66.

--- frontend/src/router/approuter.js
+++ frontend/src/router/approuter.js
@@ -1,25 +1,25 @@
-import React from "react";
-import RegisterPage from "../components/register";
-import LoginPage from "../components/login";
-import LandingPage from "../components/landing";
-import Dashboard from "../components/dashboard";
-import UserDashboard from "../components/user-dashboard";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import RegisterPage from '../components/register';
+import LoginPage from '../components/login';
+import LandingPage from '../components/landing';
+import Dashboard from '../components/dashboard';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
-const AppRouter = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" component={LandingPage} exact />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/userdashboard" component={UserDashboard} />
-        {/* <Route path='/help' component={HelpPage}/>
+const AppRouter=()=>{
+      return (
+
+            <Router>
+                  <Switch>
+                        <Route path='/' component={LandingPage} exact/>
+                        <Route path='/register' component={RegisterPage}/>
+                        <Route path="/login" component={LoginPage}/>
+                        <Route path='/dashboard' component={Dashboard}/>
+                        {/* <Route path='/help' component={HelpPage}/>
                         <Route component={PageNotFound}/> */}
-      </Switch>
-    </Router>
-  );
+                  </Switch>
+            </Router>
+
+      );
 };
 
 export default AppRouter;
