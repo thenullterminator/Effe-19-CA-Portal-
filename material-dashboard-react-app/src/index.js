6195d32778befac846b805b80b6d5aa7e20d84ed
thenullterminator <dazz2803@gmail.com>
2019-08-12 13:07:42 +0530

    Real time fetching of all tasks + admin page to add task created

--- material-dashboard-react-app/src/index.js
+++ material-dashboard-react-app/src/index.js
@@ -8,7 +8,7 @@ import Admin from "layouts/Admin.jsx";
 import Auth from "layouts/Auth.jsx";
 import RTL from "layouts/RTL.jsx";
 import Landing from "layouts/landing.js";
-
+import AdminPage from "./views/Pages/EffeAdmin";
 import "assets/css/material-dashboard-react.css?v=1.6.0";
 
 const hist = createBrowserHistory();
@@ -17,6 +17,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route exact path='/' component={Landing}/>
+      <Route exact path='/effe2019admin' component={AdminPage}/>
       <Route path="/admin" component={Admin} />
       <Route path="/auth" component={Auth} />
       <Route path="/rtl" component={RTL} />
--- material-dashboard-react-app/src/views/Dashboard/Dashboard.jsx
+++ material-dashboard-react-app/src/views/Dashboard/Dashboard.jsx
@@ -29,7 +29,7 @@ import CardHeader from "components/Card/CardHeader.jsx";
 import CardIcon from "components/Card/CardIcon.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
-
+import moment from 'moment';
 import { bugs, website, server } from "variables/general.jsx";
 
 import {
@@ -48,7 +48,8 @@ class Dashboard extends React.Component {
     this.state = {
       currentUser: {},
       isLoading: true,
-      value:0
+      value:0,
+      allTask:[],
     };
 
     firebase.auth().onAuthStateChanged(user => {
@@ -68,6 +69,35 @@ class Dashboard extends React.Component {
     });
   }
 
+  fetchAllTask=()=>{
+
+    
+    firebase.database().ref('TASKS')
+      .on('value',(snapshot)=>{
+        const AllTask=[],taskID=[];
+        var i=1;
+        snapshot.forEach((child)=>{
+
+          AllTask.push([
+            i,child.val().task,child.val().points,moment(child.val().createdAt).format('MMMM Do, YYYY')
+          ]);
+          taskID.push(child.key);
+          i++;
+        });
+        this.setState({
+          isLoading:false,
+          allTask:AllTask,
+        });
+        // console.log(AllTask);
+        // console.log(this.state.allTask);
+      });
+      
+  };
+
+  componentDidMount(){
+    this.fetchAllTask();
+  };
+  
   handleChange = (event, value) => {
     this.setState({ value });
   };
@@ -77,227 +107,231 @@ class Dashboard extends React.Component {
   };
   render() {
     const { classes } = this.props;
-    return (
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="warning" stats icon>
-                <CardIcon color="warning">
-                  <Icon>content_copy</Icon>
-                </CardIcon>
-                <p className={classes.cardCategory}>Total Regisrations</p>
-                <h3 className={classes.cardTitle}>
-                  350+
-                </h3>
-              </CardHeader>
-              <CardFooter stats>
-                <div className={classes.stats}>
-                <Update />
-                Hurry up ! Start completing tasks to win exciting prizes.
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="success" stats icon>
-                <CardIcon color="success">
-                  <Store />
-                </CardIcon>
-                <p className={classes.cardCategory}>Footfall</p>
-                <h3 className={classes.cardTitle}>+25000</h3>
-              </CardHeader>
-              <CardFooter stats>
-                <div className={classes.stats}>
-                  Effervescence-18
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="danger" stats icon>
-                <CardIcon color="danger">
-                  <Icon>info_outline</Icon>
-                </CardIcon>
-                <p className={classes.cardCategory}>Edition</p>
-                <h3 className={classes.cardTitle}>17th</h3>
-              </CardHeader>
-              <CardFooter stats>
-                <div className={classes.stats}>
-                  {/* <LocalOffer /> */}
-                  Effervescence-19
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="info" stats icon>
-                <CardIcon color="info">
-                  <Accessibility />
-                </CardIcon>
-                <p className={classes.cardCategory}>Followers</p>
-                <h3 className={classes.cardTitle}>+37000</h3>
-              </CardHeader>
-              <CardFooter stats>
-                <div className={classes.stats}>
+
+    if(this.state.isLoading) return (<h1>Loading</h1>);
+    else
+    {
+      return (
+        <div>
+          <GridContainer>
+            <GridItem xs={12} sm={6} md={3}>
+              <Card>
+                <CardHeader color="warning" stats icon>
+                  <CardIcon color="warning">
+                    <Icon>content_copy</Icon>
+                  </CardIcon>
+                  <p className={classes.cardCategory}>Total Regisrations</p>
+                  <h3 className={classes.cardTitle}>
+                    350+
+                  </h3>
+                </CardHeader>
+                <CardFooter stats>
+                  <div className={classes.stats}>
                   <Update />
-                  Just Updated
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          {/* <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="success">
-                <ChartistGraph
-                  className="ct-chart"
-                  data={dailySalesChart.data}
-                  type="Line"
-                  options={dailySalesChart.options}
-                  listener={dailySalesChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Daily Sales</h4>
-                <p className={classes.cardCategory}>
-                  <span className={classes.successText}>
-                    <ArrowUpward className={classes.upArrowCardCategory} /> 55%
-                  </span>{" "}
-                  increase in today sales.
-                </p>
-              </CardBody>
-              <CardFooter chart>
-                <div className={classes.stats}>
-                  <AccessTime /> updated 4 minutes ago
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem> */}
-          {/* <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="warning">
-                <ChartistGraph
-                  className="ct-chart"
-                  data={emailsSubscriptionChart.data}
-                  type="Bar"
-                  options={emailsSubscriptionChart.options}
-                  responsiveOptions={emailsSubscriptionChart.responsiveOptions}
-                  listener={emailsSubscriptionChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Email Subscriptions</h4>
-                <p className={classes.cardCategory}>
-                  Last Campaign Performance
-                </p>
-              </CardBody>
-              <CardFooter chart>
-                <div className={classes.stats}>
-                  <AccessTime /> campaign sent 2 days ago
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem> */}
-          {/* <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="danger">
-                <ChartistGraph
-                  className="ct-chart"
-                  data={completedTasksChart.data}
-                  type="Line"
-                  options={completedTasksChart.options}
-                  listener={completedTasksChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Completed Tasks</h4>
-                <p className={classes.cardCategory}>
-                  Last Campaign Performance
-                </p>
-              </CardBody>
-              <CardFooter chart>
-                <div className={classes.stats}>
-                  <AccessTime /> campaign sent 2 days ago
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem> */}
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={14}>
-            <CustomTabs
-              title="Tasks:"
-              headerColor="primary"
-              tabs={[
-                {
-                  tabName: "Attempted",
-                  // tabIcon: BugReport,
-                  tabContent: (
-                    <Tasks
-                      checkedIndexes={[0, 3]}
-                      tasksIndexes={[0, 1, 2, 3]}
-                      tasks={bugs}
-                    />
-                  )
-                },
-                {
-                  tabName: "Not Attempted",
-                  // tabIcon: Code,
-                  tabContent: (
-                    <Tasks
-                      checkedIndexes={[0]}
-                      tasksIndexes={[0, 1]}
-                      tasks={website}
-                    />
-                  )
-                }
-                // ,
-                // {
-                //   tabName: "Server",
-                //   tabIcon: Cloud,
-                //   tabContent: (
-                //     <Tasks
-                //       checkedIndexes={[1]}
-                //       tasksIndexes={[0, 1, 2]}
-                //       tasks={server}
-                //     />
-                //   )
-                // }
-              ]}
-            />
-          </GridItem>
+                  Hurry up ! Start completing tasks to win exciting prizes.
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem>
+            <GridItem xs={12} sm={6} md={3}>
+              <Card>
+                <CardHeader color="success" stats icon>
+                  <CardIcon color="success">
+                    <Store />
+                  </CardIcon>
+                  <p className={classes.cardCategory}>Footfall</p>
+                  <h3 className={classes.cardTitle}>+25000</h3>
+                </CardHeader>
+                <CardFooter stats>
+                  <div className={classes.stats}>
+                    Effervescence-18
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem>
+            <GridItem xs={12} sm={6} md={3}>
+              <Card>
+                <CardHeader color="danger" stats icon>
+                  <CardIcon color="danger">
+                    <Icon>info_outline</Icon>
+                  </CardIcon>
+                  <p className={classes.cardCategory}>Edition</p>
+                  <h3 className={classes.cardTitle}>17th</h3>
+                </CardHeader>
+                <CardFooter stats>
+                  <div className={classes.stats}>
+                    {/* <LocalOffer /> */}
+                    Effervescence-19
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem>
+            <GridItem xs={12} sm={6} md={3}>
+              <Card>
+                <CardHeader color="info" stats icon>
+                  <CardIcon color="info">
+                    <Accessibility />
+                  </CardIcon>
+                  <p className={classes.cardCategory}>Followers</p>
+                  <h3 className={classes.cardTitle}>+37000</h3>
+                </CardHeader>
+                <CardFooter stats>
+                  <div className={classes.stats}>
+                    <Update />
+                    Just Updated
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem>
           </GridContainer>
           <GridContainer>
-          <GridItem xs={12} sm={12} md={14}>
-            <Card>
-              <CardHeader color="warning">
-                <h4 className={classes.cardTitleWhite}>All Tasks</h4>
-                <p className={classes.cardCategoryWhite}>
-                  New tasks will appear in top of the table.
-                </p>
-              </CardHeader>
-              <CardBody>
-                <Table
-                  tableHeaderColor="warning"
-                  tableHead={["ID", "Name", "Base points", "Date"]}
-                  tableData={[
-                    ["1", "Dakota Rice", "$36,738", "Niger"],
-                    ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-                    ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-                    ["4", "Philip Chaney", "$38,735", "Korea, South"]
-                  ]}
-                />
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    );
+            {/* <GridItem xs={12} sm={12} md={4}>
+              <Card chart>
+                <CardHeader color="success">
+                  <ChartistGraph
+                    className="ct-chart"
+                    data={dailySalesChart.data}
+                    type="Line"
+                    options={dailySalesChart.options}
+                    listener={dailySalesChart.animation}
+                  />
+                </CardHeader>
+                <CardBody>
+                  <h4 className={classes.cardTitle}>Daily Sales</h4>
+                  <p className={classes.cardCategory}>
+                    <span className={classes.successText}>
+                      <ArrowUpward className={classes.upArrowCardCategory} /> 55%
+                    </span>{" "}
+                    increase in today sales.
+                  </p>
+                </CardBody>
+                <CardFooter chart>
+                  <div className={classes.stats}>
+                    <AccessTime /> updated 4 minutes ago
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem> */}
+            {/* <GridItem xs={12} sm={12} md={4}>
+              <Card chart>
+                <CardHeader color="warning">
+                  <ChartistGraph
+                    className="ct-chart"
+                    data={emailsSubscriptionChart.data}
+                    type="Bar"
+                    options={emailsSubscriptionChart.options}
+                    responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                    listener={emailsSubscriptionChart.animation}
+                  />
+                </CardHeader>
+                <CardBody>
+                  <h4 className={classes.cardTitle}>Email Subscriptions</h4>
+                  <p className={classes.cardCategory}>
+                    Last Campaign Performance
+                  </p>
+                </CardBody>
+                <CardFooter chart>
+                  <div className={classes.stats}>
+                    <AccessTime /> campaign sent 2 days ago
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem> */}
+            {/* <GridItem xs={12} sm={12} md={4}>
+              <Card chart>
+                <CardHeader color="danger">
+                  <ChartistGraph
+                    className="ct-chart"
+                    data={completedTasksChart.data}
+                    type="Line"
+                    options={completedTasksChart.options}
+                    listener={completedTasksChart.animation}
+                  />
+                </CardHeader>
+                <CardBody>
+                  <h4 className={classes.cardTitle}>Completed Tasks</h4>
+                  <p className={classes.cardCategory}>
+                    Last Campaign Performance
+                  </p>
+                </CardBody>
+                <CardFooter chart>
+                  <div className={classes.stats}>
+                    <AccessTime /> campaign sent 2 days ago
+                  </div>
+                </CardFooter>
+              </Card>
+            </GridItem> */}
+          </GridContainer>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={14}>
+              <CustomTabs
+                title="Tasks:"
+                headerColor="primary"
+                tabs={[
+                  {
+                    tabName: "Attempted",
+                    // tabIcon: BugReport,
+                    tabContent: (
+                      <Tasks
+                        checkedIndexes={[0, 3]}
+                        tasksIndexes={[0, 1, 2, 3]}
+                        tasks={bugs}
+                      />
+                    )
+                  },
+                  {
+                    tabName: "Not Attempted",
+                    // tabIcon: Code,
+                    tabContent: (
+                      <Tasks
+                        checkedIndexes={[0]}
+                        tasksIndexes={[0, 1]}
+                        tasks={website}
+                      />
+                    )
+                  }
+                  // ,
+                  // {
+                  //   tabName: "Server",
+                  //   tabIcon: Cloud,
+                  //   tabContent: (
+                  //     <Tasks
+                  //       checkedIndexes={[1]}
+                  //       tasksIndexes={[0, 1, 2]}
+                  //       tasks={server}
+                  //     />
+                  //   )
+                  // }
+                ]}
+              />
+            </GridItem>
+            </GridContainer>
+            <GridContainer>
+            <GridItem  xs={12} sm={12} md={14}>
+              <Card>
+                <CardHeader color="warning">
+                  <h4 className={classes.cardTitleWhite}>All Tasks</h4>
+                  <p className={classes.cardCategoryWhite}>
+                    New tasks will appear at the end of the table.
+                  </p>
+                </CardHeader>
+                <CardBody>
+                    <Table
+                    tableHeaderColor="warning"
+                    tableHead={["ID", "Name", "Base points", "Date"]}
+                    tableData={
+                      this.state.allTask
+                    }
+                  />
+                </CardBody>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+      );
+    }
+
+    
   }
 }
 
